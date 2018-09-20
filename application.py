c6d79f1e931e7b9e2fcace2657e8518c2cbec5cb
Karl Horning <karl_horning@Karls-iMac.local>
2018-09-20 20:07:06 +0100

    Added the login and account routes + logic

--- application.py
+++ application.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
-from flask_login import LoginManager, login_required
+from flask import Flask, redirect,  render_template, request, url_for
+from flask_login import LoginManager, login_required, login_user
+
+from config import Config
+from mockdbhelper import MockDBHelper as DBHelper
+from models import User
 
 app = Flask(__name__)
+app.secret_key = Config.SECRET_KEY
 login_manager = LoginManager(app)
+
+db = DBHelper()
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = db.get_user(user_id)
+    if user_password:
+        return User(user_id)
 
 @app.route('/')
 def index():
@@ -12,6 +25,18 @@ def index():
 @login_required
 def account():
     return 'You are logged in!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user_password = db.get_user(email)
+
+    if user_password and user_password == password:
+        user = User(email)
+        login_user(user)
+        return redirect(url_for('account'))
+    return index()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
