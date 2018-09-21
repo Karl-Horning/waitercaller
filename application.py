d1962495eb6ddcabeaf21fdc8c33e3a5860dec51
Karl Horning <karl_horning@Karls-iMac.local>
2018-09-21 10:45:50 +0100

    Password hash and salt added; autopep8 used to format files

--- application.py
+++ application.py
@@ -1,47 +1,55 @@
-from flask import Flask, redirect,  render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for
 from flask_login import LoginManager, login_required, login_user, logout_user
 
 from config import Config
 from mockdbhelper import MockDBHelper as DBHelper
 from models import User
+from passwordhelper import PasswordHelper
 
 app = Flask(__name__)
 app.secret_key = Config.SECRET_KEY
 login_manager = LoginManager(app)
 
-db = DBHelper()
+DB = DBHelper()
+PH = PasswordHelper()
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    user_password = db.get_user(user_id)
+    user_password = DB.get_user(user_id)
     if user_password:
         return User(user_id)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/account')
 @login_required
 def account():
     return 'You are logged in!'
 
+
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    user_password = db.get_user(email)
+    stored_user = DB.get_user(email)
 
-    if user_password and user_password == password:
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
     return index()
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
